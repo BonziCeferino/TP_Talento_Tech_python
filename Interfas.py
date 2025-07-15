9ae3fe6109f582c527ccb9f52fd6f76dd4f8fd22
BonziCeferino <157132803+BonziCeferino@users.noreply.github.com>
2025-07-15 11:21:52 -0300

    Update Interfas.py
    Se mejora interfas grafica

--- Interfas.py
+++ Interfas.py
@@ -5,7 +5,10 @@ init(autoreset=True)                           # Para que los colores se resetee
 ancho = os.get_terminal_size().columns         # Grabo en variable el ancho del terminal
 fondo = Back.BLUE + Style.BRIGHT + Fore.BLACK
 
-def centrar_texto(texto, ancho):
+def limpiar_pantalla():                        # Para limpiar la pantalla y mejorar visual
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def centrar_texto(texto, ancho):               # Centrar todo automaticamente
     texto = str(texto)
     espacios_totales = max(ancho - len(texto), 0)
     izquierda = espacios_totales // 2
@@ -16,49 +19,25 @@ def centrar_texto(texto, ancho):
 def relleno(color_fondo,ancho):                      # Para rellenar espacios
     print(color_fondo + " " * ancho) 
 
-def mensaje_bienvenida():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    texto = "BIENVENIDO AL SISTEMA DE INVENTARIO"
-    relleno(fondo,ancho)
+def estilo(texto):                                    # Estilo a usar 
     relleno(fondo,ancho)
     print(fondo + centrar_texto(texto, ancho))                                                      
     relleno(fondo,ancho)
-    relleno(fondo,ancho) 
+
+def mensaje_bienvenida():
+    limpiar_pantalla()
+    texto = "BIENVENIDO AL SISTEMA DE INVENTARIO"
+    estilo(texto) 
 
 def menu_inicio(nombre):
-    os.system('cls')
-    print("\n")
-    print("************************************************************")
-    print("+++++++++++++++  Programa de compras Online ++++++++++++++++")
-    print("************************************************************\n")
-    print(f"+++++++++++++++++++++ Bienvenido {nombre} +++++++++++++++++\n")
-    print("  1. Agregar producto                 2. Mostrar productos  \n")
-    print("  3. Buscar producto                  4. Eliminar producto  \n")
-    print("  5. Stock Minimo                     6. SALIR              \n")
-    print("------------------Ingrese la opción deseada-----------------")
+    limpiar_pantalla()
+    estilo(f"Progama de Compras Online Bienvenido {nombre}")
+    estilo("1. Agregar producto                 2. Mostrar productos")
+    estilo("3. Buscar producto                  4. Eliminar producto")
+    estilo("5. Stock Minimo                     6. SALIR             ")
+    estilo("Ingrese la opción deseada")
 
 def iniciar_sesion():
     texto = "| Iniciar Sesion (I) |  Registrarse  R  |      Salir  X      |"
-    relleno(fondo,ancho)
-    relleno(fondo,ancho)
-    print(fondo + centrar_texto(texto, ancho))                                                      
-    relleno(fondo,ancho)
-    relleno(fondo,ancho) 
+    estilo(texto)
 
-"""def menu_inicio(nombre="Usuario"):
-    fondo = Back.LIGHTCYAN_EX + Fore.BLACK + Style.BRIGHT
-    asteriscos = Fore.MAGENTA + "*" * 60
-
-    os.system('cls' if os.name == 'nt' else 'clear')
-    print("\n" * 3)
-    print(centrar_texto(asteriscos))
-    print(centrar_texto(fondo + " PROGRAMA DE COMPRAS ONLINE ".center(60)))
-    print(centrar_texto(asteriscos))
-    print()
-    print(centrar_texto(fondo + f"Bienvenido {nombre}".center(60)))
-    print()
-    print(centrar_texto(fondo + "1. Agregar producto    2. Mostrar productos".center(60)))
-    print(centrar_texto(fondo + "3. Buscar producto     4. Eliminar producto".center(60)))
-    print(centrar_texto(fondo + "5. SALIR".center(60)))
-    print()
-    print(centrar_texto(fondo + "Ingrese la opción deseada:".center(60)))"""
